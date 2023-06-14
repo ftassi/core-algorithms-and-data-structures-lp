cf87373f28f63daeda7441bf4559f9b27a8ccdb1
Francesco Tassi <tassi.francesco@gmail.com>
2023-06-14 10:19:45 +0200

    Provisional: add commands to main

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ mod core;
 mod errors;
 mod trading_platform;
 mod tx;
-use crate::accounting::Accounts;
+use crate::{accounting::Accounts, trading_platform::TradingPlatform, core::Order};
 
 fn read_from_stdin(label: &str) -> String {
     let mut buffer = String::new();
@@ -19,18 +19,33 @@ fn read_from_stdin(label: &str) -> String {
 fn main() {
     println!("Hello, accounting world!");
 
-    let mut ledger = Accounts::new();
+    let mut trading_platform = TradingPlatform::new();
     loop {
         let input = read_from_stdin(
             "Choose operation [deposit, withdraw, send, print, quit], confirm with return:",
         );
         match input.as_str() {
+            "order" => {
+                // let price = read_from_stdin("Price:").parse();
+                // let amount = read_from_stdin("Amount:").parse();
+                // let side = read_from_stdin("Side:").parse();
+                // let signer = read_from_stdin("Signer:").parse();
+                //
+                // trading_platform.order(Order{
+                //     price,
+                //     amount,
+                //     side,
+                //     signer
+                // });
+            }
+            "orderbook" => {}
+            "txlog" => {}
             "deposit" => {
                 let account = read_from_stdin("Account:");
 
                 let raw_amount = read_from_stdin("Amount:").parse();
                 if let Ok(amount) = raw_amount {
-                    let _ = ledger.deposit(&account, amount);
+                    let _ = trading_platform.deposit(&account, amount);
                     println!("Deposited {} into account '{}'", amount, account)
                 } else {
                     eprintln!("Not a number: '{:?}'", raw_amount);
@@ -40,7 +55,7 @@ fn main() {
                 let account = read_from_stdin("Account:");
                 let raw_amount = read_from_stdin("Amount:").parse();
                 if let Ok(amount) = raw_amount {
-                    let _ = ledger.withdraw(&account, amount);
+                    let _ = trading_platform.withdraw(&account, amount);
                 } else {
                     eprintln!("Not a number: '{:?}'", raw_amount);
                 }
@@ -50,13 +65,13 @@ fn main() {
                 let recipient = read_from_stdin("Recipient Account:");
                 let raw_amount = read_from_stdin("Amount:").parse();
                 if let Ok(amount) = raw_amount {
-                    let _ = ledger.send(&sender, &recipient, amount);
+                    let _ = trading_platform.send(&sender, &recipient, amount);
                 } else {
                     eprintln!("Not a number: '{:?}'", raw_amount);
                 }
             }
             "print" => {
-                println!("The ledger: {:?}", ledger);
+                println!("The ledger: {:?}", trading_platform);
             }
             "quit" => {
                 println!("Quitting...");
